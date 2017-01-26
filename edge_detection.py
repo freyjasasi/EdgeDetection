96a64d7942630dee808999f03e37241af0c140e1
freyjasasi <freyjasasi@gmail.com>
2017-01-26 22:41:41 +0530

    Add files via upload

---
+++ edge_detection.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan  7 09:37:32 2017
+
+@author: sasi
+"""
+
+import cv2
+import numpy as np
+
+webcam=cv2.VideoCapture(0)
+
+while(webcam.isOpened()):
+    _,frame=webcam.read()
+    
+    laplacian=cv2.Laplacian(frame,cv2.CV_64F)
+#    sobelx=cv2.Sobel(frame,cv2.CV_64F,1,0,ksize=5)
+#    sobely=cv2.Sobel(frame,cv2.CV_64F,0,1,ksize=5)
+    edges=cv2.Canny(frame,100,90)
+    
+    cv2.imshow("laplacian",laplacian)
+    cv2.imshow("color img",frame)
+#    cv2.imshow("sobelx",sobelx)
+#    cv2.imshow("sobely",sobely)
+    cv2.imshow("edges",edges)
+    if(cv2.waitKey(1)&0xff==ord("c")):
+        break
+webcam.release()
+cv2.destroyAllWindows()
